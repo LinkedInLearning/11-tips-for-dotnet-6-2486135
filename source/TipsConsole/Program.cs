8d34a79b32dc5f11b3e814760749641e5a10aadc
Walt Ritscher <waltritscher@outlook.com>
2022-05-10 18:21:51 -0700

    Add CommandLineAPI and DragonFruit NuGet Packages

---
+++ source/TipsConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TipsConsole {
+	internal class Program {
+		// string array is the standard way to get
+		// arguments into the application
+
+		// use the 
+		static void Main(string[] args) {
+			
+		}
+	}
+}
