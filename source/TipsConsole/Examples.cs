93d453837903d106a36b615e05245cab70f34a65
walt-instructor <staffauthor@outlook.com>
2022-05-21 15:22:28 -0700

    update comments, add GroupBy example

--- source/TipsConsole/Examples.cs
+++ source/TipsConsole/Examples.cs
@@ -6,41 +6,45 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TipsConsole {
-  internal class Examples {
-    public void RunExample() {
-      // GroupBy and ToLookup return a collection that
-      // has a key and an inner collection based on a key field value.
+	internal class Examples {
+		public void RunExample() {
+			// GroupBy and ToLookup return a sequence that
+			// has a key (the group) and an inner sequence based on a key field value.
 
-      // GroupBy is an IQueryable, with deferred execution
-      // ToLookup is an IEnumerable with immediate execution
 
-      var cardSource = new CardSource();
-      var collectibles = cardSource.CollectibleCards;
+			var cardSource = new CardSource();
+			var collectibles = cardSource.CollectibleCards;
+			
+			// ILookup vs Dictionary, both hold key and value items
+			// ILookup is a IEnumerable<Key, IEnumerable>,
+			// think of it as a dictionary with IEnumerable<T> values
 
-      // ILookup vs Dictionary, both hold key and value items
-      // Ilookup value is a IEnumerable
-      // ILookup is immutable, no .Add method for example
-      // ILookup supports null keys
-      // Lookup by key returns an empty sequence (not an exception!)
-      ILookup<CardFamily, CollectableCard> lookupByCardFamily = collectibles.ToLookup(c => c.CardFamily);
-      var lookupByPrice = collectibles.ToLookup(c => c.IsDiscounted);
-   
+			// ILookup is immutable, no .Add method for example
+			// ILookup supports null keys
+			// Lookup by null key returns an empty sequence (not an exception!)
+			ILookup<CardFamily, CollectableCard> lookupByCardFamily;
+			lookupByCardFamily = collectibles.ToLookup(c => c.CardFamily);
+			var lookupByPrice = collectibles.ToLookup(c => c.IsDiscounted);
+			var grouped = collectibles.GroupBy(x => x.CardFamily);
 
-      // 
-      foreach (var family in lookupByCardFamily)
-      {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine(family.Key);
-        // full price cards
-        Console.ForegroundColor = ConsoleColor.Green;
-        foreach (var card in family)
-        {
-          Console.WriteLine($"  { card.Name}");
-        }
-      }
-      var animals = lookupByCardFamily[CardFamily.Animals];
-    }
-  }
+			// Lookup by null key returns an empty sequence (not an exception!)
+			var animals = lookupByCardFamily[CardFamily.Animals];
+
+			// 
+			foreach (var family in lookupByCardFamily)
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine(family.Key);
+				// show by CardFamily
+				Console.ForegroundColor = ConsoleColor.Green;
+				foreach (var card in family)
+				{
+					Console.WriteLine($"  { card.Name}");
+				}
+			}
+
+		}
+	}
 
 
 }
