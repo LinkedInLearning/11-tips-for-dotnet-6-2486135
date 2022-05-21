f6db2c0c5d3ed433954945635ec2cd7c2211622f
walt-instructor <staffauthor@outlook.com>
2022-05-21 14:33:02 -0700

    add color console code

--- source/TipsConsole/Examples.cs
+++ source/TipsConsole/Examples.cs
@@ -5,46 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TipsConsole {
-    internal class Examples {
-        public void FlattenLists() {
-            var brandA = new Brand { Name = "Fancy-Shoes", Colors = new List<string> { "Red", "Orange" } };
-            var brandB = new Brand { Name = "Lux-Cars", Colors = new List<string> { "Gold", "Silver" } };
-            var brandC = new Brand { Name = "Wow-Electronics", Colors = new List<string> { "Black", "Purple", "Gold" } };
+	internal class Examples {
+		public void FlattenLists() {
+			var brandA = new Brand { Name = "Fancy-Shoes", Colors = new List<string> { "Red", "Orange" } };
+			var brandB = new Brand { Name = "Lux-Cars", Colors = new List<string> { "Gold", "Silver" } };
+			var brandC = new Brand { Name = "Wow-Electronics", Colors = new List<string> { "Black", "Purple", "Gold" } };
 
-            List<Brand> brands = new List<Brand>();
-            brands.Add(brandA);
-            brands.Add(brandB);
-            brands.Add(brandC);
+			List<Brand> brands = new List<Brand>();
+			brands.Add(brandA);
+			brands.Add(brandB);
+			brands.Add(brandC);
 
-            // there are seven colors in the .Colors property
+			// there are seven colors in the .Colors property
 
-            // Flatten a multi-dimension set into a single set
-            // IN .NET use LINQ .SelectMany or .ContinueWith
+			// Flatten a multi-dimension set into a single set
+			// IN .NET use LINQ .SelectMany or .ContinueWith
 
-            // this works, but is not optimal
+			// this works, but is not optimal
+			List<List<string>> colorsA = brands.Select(x => x.Colors).ToList();
+			Console.WriteLine(colorsA);
 
-            List<List<string>> colorsA = brands.Select(x => x.Colors).ToList();
-            Console.WriteLine(colorsA);
+			foreach (var colors in colorsA)
+			{
+				foreach (var color in colors)
+				{
+					Console.WriteLine(color);
+				}
+			}
 
-            foreach (var colors in colorsA)
-            {
-                foreach (var color in colors)
-                {
-                    Console.WriteLine(color);
-                }
-            }
-            // flatten
+			Console.WriteLine("------------");
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			// flatten
 
-            List<string> colorsB = brands.SelectMany(x => x.Colors).ToList();
-            foreach (var color in colorsB)
-            {
-                Console.WriteLine(color);
-            }
-        }
-    }
+			List<string> colorsB = brands.SelectMany(x => x.Colors).ToList();
+			foreach (var color in colorsB)
+			{
+				Console.WriteLine(color);
+			}
 
-    public class Brand {
-        public string Name { get; set; }
-        public List<string> Colors { get; set; }
-    }
+			Console.ForegroundColor = ConsoleColor.White;
+		}
+	}
+
+	public class Brand {
+		public string Name { get; set; }
+		public List<string> Colors { get; set; }
+	}
 }
