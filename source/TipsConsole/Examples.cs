15656b91da9ee4a5ea91c6e453875d9647c9604d
Walt Ritscher <waltritscher@outlook.com>
2022-05-20 21:44:08 -0700

    Path examples

--- source/TipsConsole/Examples.cs
+++ source/TipsConsole/Examples.cs
@@ -7,8 +7,26 @@ using System.Threading.Tasks;
 namespace TipsConsole {
     internal class Examples {
         public void RunExample() {
-           
-        }
+
+      Console.WriteLine("Get file path for Linux or Windows.");
+      Console.WriteLine();
+      string path = @"c:\readme.txt";
+      string path2 = @"c:\\files\images\logo.png";
+      string path3 = @"c:\\files\images\..\sales.xlsx";
+
+      string path4 = @"files\images\banner.jpg";
+
+
+      string path5 = @"\files\music\song.wav";
+      string path6 = @".\files\playlists\list.mpu";
+      Console.WriteLine(path.MakePathString());
+      Console.WriteLine(path2.MakePathString());
+      Console.WriteLine(path3.MakePathString());
+      Console.WriteLine(path4.MakePathString());
+      Console.WriteLine(path5.MakePathString());
+      Console.WriteLine(path6.MakePathString());
+
+    }
     }
 
 }
---
+++ source/TipsConsole/PathExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipsConsole {
+  internal static class PathExtensions {
+
+    public static string MakePathString(this string candidatePath) {
+
+      if (candidatePath.IsAbsolutePath())
+        return Path.GetFullPath(candidatePath);
+      var fakeRoot = Environment.CurrentDirectory; // Get a cross platform full path
+      var combined = Path.Combine(fakeRoot, candidatePath);
+      combined = Path.GetFullPath(combined);
+      return combined.RelativeTo(fakeRoot);
+    }
+
+    // For platform consistency it is best to use the Path.DirectorySeparatorChar
+    // to determine the proper character to use when deploying apps on different platforms.
+    private static bool IsAbsolutePath(this string candidatePath) {
+      ArgumentNullException.ThrowIfNull(candidatePath, nameof(candidatePath));
+
+      var isRooted = Path.IsPathRooted(candidatePath);
+      var isSomething = !Path.GetPathRoot(candidatePath).Equals(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal);
+      var isAnother = !Path.GetPathRoot(candidatePath).Equals(Path.AltDirectorySeparatorChar.ToString(), StringComparison.Ordinal);
+
+
+      return isRooted && isSomething && isAnother;
+
+    }
+
+    private static string RelativeTo(this string filespec, string folder) {
+      var pathUri = new Uri(filespec);
+
+      // Include an end slash on the path
+      if (folder.EndsWith(Path.DirectorySeparatorChar.ToString()) == false)
+      {
+        folder += Path.DirectorySeparatorChar;
+      }
+      var folderUri = new Uri(folder);
+      return Uri.UnescapeDataString(folderUri.MakeRelativeUri(pathUri).ToString()
+          .Replace('/', Path.DirectorySeparatorChar));
+    }
+  }
+}
