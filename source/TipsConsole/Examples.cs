fe924c78aab68cc7854fe34cf14ff551c8f4176b
Walt Ritscher <waltritscher@outlook.com>
2022-05-22 21:21:22 -0700

    Add example code

--- source/TipsConsole/Examples.cs
+++ source/TipsConsole/Examples.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TipsConsole {
-    internal class Examples {
-        public void RunExample() {
-           
-        }
+  internal class Examples {
+    public void RunExample() {
+      var top = new TopThreeHourly();
+      foreach (var sensor in top.FlowersSold)
+      {
+        Console.WriteLine(sensor);
+      }
+
+      ((List<string>)top.FlowersSold).Add("Zinnia");
+      Console.ForegroundColor = ConsoleColor.Cyan;
+      Console.WriteLine("--------");
+      foreach (var sensor in top.FlowersSold)
+      {
+        Console.WriteLine(sensor);
+      }
+
+      Console.ForegroundColor = ConsoleColor.White;
     }
+  }
 
 }
---
+++ source/TipsConsole/TopThree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipsConsole {
+  internal class TopThreeHourly {
+    private List<string> _flowersSold;
+    private List<string> _treesSold;
+    public TopThreeHourly() {
+      _flowersSold = new List<string>(capacity:3);
+      _treesSold = new List<string>(capacity: 3);
+      _flowersSold.Add("Amber Rose");
+      _flowersSold.Add("Bluebell");
+
+      _flowersSold.Add("Calla Lily");
+
+      _treesSold.Add("Alder");
+      _treesSold.Add("Birch");
+      _treesSold.Add("Cedar");
+
+
+    }
+
+    public IEnumerable<string> FlowersSold
+    {
+      get
+      {
+
+        return _flowersSold.AsEnumerable();
+      }
+    }
+
+    public IReadOnlyCollection<string> TreesSold
+    {
+      get
+      {
+
+        return new ReadOnlyCollection<string>(_treesSold);
+      }
+    }
+  }
+}
